f132a70423aac0ee9d3fbb38607eb0b51597e62a
Przemyslaw Kula <83509@student.ue.poznan.pl>
2024-10-14 22:13:54 +0200

    Task 0 done

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+documents = []
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+num_of_docs = int(input("Ile dokumentów chcesz wprowadzić? "))
+
+for i in range(num_of_docs):
+    document = input(f"Podaj treść dokumentu {i}: ")
+    documents.append(document)
+
+search_word = input("Podaj słowo, którego wystąpienia chcesz policzyć: ").lower()
+
+document_word_count = []
+
+for index, doc in enumerate(documents):
+    
+    words = doc.lower().split()
+    
+    word_count = words.count(search_word)
+    
+    document_word_count.append((index, word_count))
+
+sorted_documents = sorted(document_word_count, key=lambda x: x[1], reverse=True)
+
+print('\nTen wyraz został wprowadzony: ', search_word)
+print("\nDokumenty uszeregowane według liczby wystąpień słowa:")
+sorted_document_numbers = [doc[0] for doc in sorted_documents]
+print(sorted_document_numbers)
